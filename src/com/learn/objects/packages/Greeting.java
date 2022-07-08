cb69c814325a69e57f536cfc0ca5e978b307f9c6
theabhayprajapati <theabhayprajapati@gmail.com>
2022-07-08 22:18:54 +0530

    created a packages folder with greeting and meeting files and did static imports

---
+++ src/com/learn/objects/packages/Greeting.java
@@ -0,0 +1,11 @@
+package com.learn.objects.packages;
+
+import static com.learn.objects.packages.Meeting.greeting;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("this is Greeting");
+        greeting();
+    }
+
+}
---
+++ src/com/learn/objects/packages/Meeting.java
@@ -0,0 +1,12 @@
+package com.learn.objects.packages;
+
+public class Meeting    {
+    public static void main(String[] args) {
+        System.out.println("this is Meeting");
+        greeting();
+    }
+
+    public static void greeting() {
+        System.out.println("from Meeting greeting methods");
+    }
+}
