1e24d319a13070cc261559460f76a6b59cf6c3bb
theabhayprajapati <theabhayprajapati@gmail.com>
2022-07-08 23:07:14 +0530

    started with encp i have an idea now

---
+++ src/com/learn/objects/principles/Encap.java
@@ -0,0 +1,8 @@
+package com.learn.objects.principles;
+
+public class Encap {
+    public static void main(String[] args) {
+        System.out.println("this is Encap");
+        System.out.println("here the value of variables are hidden");
+    }
+}
