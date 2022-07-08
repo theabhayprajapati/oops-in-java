357137b88010fc95687e8905cc80d757188d537c
theabhayprajapati <theabhayprajapati@gmail.com>
2022-07-08 22:15:22 +0530

    created a class Student with static var totalStudents

--- src/com/learn/objects/StudentClass.java
+++ src/com/learn/objects/StudentClass.java
@@ -10,6 +10,11 @@ public class StudentClass {
 //        this is quite repetitive right.
         Student Arin = new Student(2423424, "Arin Prajapati", 98);
         System.out.println(Arin.name);
+        /*print total students*/
+        /*why is this not working... : I had made a static variable in class Student; */
+        /*as i was using wrong name rather totalStudent i was using totalStudents : go fixed*/
+
+        System.out.println(Student.totalStudents);
     }
 
 }
@@ -18,11 +23,13 @@ class Student {
     int id;
     String name;
     int marks;
+    static int totalStudents = 0;
     /* so this would be repetitive we are making constructors*/
     Student(int id, String name, int marks) {
         this.id  = id;
         this.name = name;
         this.marks = marks;
+        totalStudents++;
     }
 
 }
