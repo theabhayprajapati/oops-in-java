d1188a8660644f911261eb489d5271dba53deaef
theabhayprajapati <theabhayprajapati@gmail.com>
2022-07-14 22:11:01 +0530

    add insert feature

--- src/com/learn/objects/LinkedList/LL.java
+++ src/com/learn/objects/LinkedList/LL.java
@@ -7,17 +7,31 @@ public class LL {
         private Node(int value){
             this.value = value;
         }
+        private Node(int value, Node next){
+            this.value = value;
+            this.next=next;
+        }
     }
     private Node head;
     private Node tail;
     private int size;
-    public void insert(int value){
+    public void inserthead(int value){
         Node node = new Node(value);
         node.next = head;
         head = node;
         if(tail==null){
             tail=head;
         }
+        size++;
+    }
+    public void inserTail(int value){
+        if(tail ==null){
+            inserthead(value);
+            return;
+        }
+        Node node = new Node(value);
+        tail.next = node;
+        tail=node;
         size++;
     }
     public void display(){
@@ -28,4 +42,20 @@ public class LL {
         }
         System.out.println(" END ");
     }
+
+    public int getSize() {
+        return size;
+    }
+    public void insert(int index, int value){
+        if(index==0){
+            inserthead(value);
+        }
+        Node last =head;
+        for(int i = 0; i<index -2; i++){
+            last =head.next;
+        }
+        last.next= new Node(value, last.next);
+        size++;
+    }
+
 }
--- src/com/learn/objects/LinkedList/Main.java
+++ src/com/learn/objects/LinkedList/Main.java
@@ -3,12 +3,13 @@ package com.learn.objects.LinkedList;
 public class Main {
     public static void main(String[] args) {
         LL list = new LL();
-        list.insert(12);
-        list.insert(13);
-        list.insert(83);
-        for(int i =0; i<=10; i++){
-            list.insert(i*2);
-        }
+        list.inserthead(12);
+        list.inserthead(13);
+        list.inserthead(83);
         list.display();
+        System.out.println(list.getSize());
+        list.insert(2, 2);
+        list.display();
+        System.out.println(list.getSize());
     }
 }
